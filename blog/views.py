733c056f70b299af813c706803c9ea15f4c44bf6
QD <1337Iaah@gmail.com>
2020-09-13 19:40:23 +0200

    db1
    database in place instead dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    },
-    {
-        'author': 'kacper',
-        'title':'kacperek',
-        'content':'kac',
-        'date_posted': 'xx xx xx'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
